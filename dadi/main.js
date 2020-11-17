90c087086eb77e66c3ffd51f4d3f0fca665d9df7
andreamazza84 <andrea.mazza84@gmail.com>
2020-11-17 18:25:49 +0100

    Gioco dei dadi: ancora qualcosa da sistemare

--- dadi/main.js
+++ dadi/main.js
@@ -1,15 +1,6 @@
 //Gioco dei dadi
 //Generare un numero random da 1  a 6, sia per il giocatore sia per il computer
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
-//Prima di partire a scrivere codice poniamoci qualche domanda:
-//Che ci sia un array da qualche parte?
-//Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-//Consigli del giorno:
-//1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-//2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-//3. si ma noi cosa vogliamo fare?
-//4. torniamo a scrivere in italiano
-//5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 
 //REGOLE DEL GIOCO (utente vs computer)
 //Al click del bottone il giocatore lancia il dado una volta.
@@ -27,9 +18,29 @@ toss.addEventListener("click", function(){
 
   playerToss[j] = parseInt((Math.floor(Math.random() * 10) / 10 * 6));
   computerToss[j] = parseInt((Math.floor(Math.random() * 10) / 10 * 6));
-  j = ++j;
-  console.log("j: " + j);
-  if(j == 6){
+
+  var playerTable = [
+    document.getElementById("player-result-1"),
+    document.getElementById("player-result-2"),
+    document.getElementById("player-result-3"),
+    document.getElementById("player-result-4"),
+    document.getElementById("player-result-5"),
+    document.getElementById("player-result-6")
+  ];
+  var computerTable = [
+    document.getElementById("computer-result-1"),
+    document.getElementById("computer-result-2"),
+    document.getElementById("computer-result-3"),
+    document.getElementById("computer-result-4"),
+    document.getElementById("computer-result-5"),
+    document.getElementById("computer-result-6")
+  ];
+
+  playerTable[j].innerHTML = playerToss[j];
+  computerTable[j].innerHTML = computerToss[j];
+
+
+  if(j == 5){
     j = 0; //reset
 
     for (var i = 0; i < playerToss.length; i++) {
@@ -37,12 +48,9 @@ toss.addEventListener("click", function(){
       computerResult = computerResult + computerToss[i];
       result = playerResult > computerResult;
     }
-    // console.log("lancio giocatore " + playerToss[0]);
-    // console.log("lancio computer " + playerToss[0]);
-    //
-    // console.log("giocatore " + playerResult);
-    // console.log("computer " + computerResult);
-    // /FOR
+    
+    document.getElementById("player-tot").innerHTML = playerResult;
+    document.getElementById("computer-tot").innerHTML = computerResult;
 
     if(result){
       alert("Hai vinto!");
@@ -51,4 +59,7 @@ toss.addEventListener("click", function(){
       alert("Hai perso!");
     } // /IF
   }// /IF
+
+
+  j = ++j;
 });
